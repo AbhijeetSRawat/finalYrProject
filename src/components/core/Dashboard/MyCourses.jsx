827a17d0b81c207becdc462b8598824ce6ad59ce
Abhijeet Singh Rawat <rawatab2@gmail.com>
2025-04-28 11:50:55 +0530

    first commit

---
+++ src/components/core/Dashboard/MyCourses.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import { VscAdd } from "react-icons/vsc"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+import { fetchInstructorCourses } from "../../../services/operations/courseDetailsAPI"
+import IconBtn from "../../Common/IconBtn"
+import CoursesTable from "./InstructorCourses/CoursesTable"
+
+export default function MyCourses() {
+  const { token } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+  const [courses, setCourses] = useState([]);
+
+  const fetchCourses = async (token) => {
+
+    console.log("TOKEN => ",token);
+
+    const result = await fetchInstructorCourses(token);
+    if (result) {
+      setCourses(result)
+    }
+  }
+
+  useEffect(() => {
+    
+    fetchCourses(token);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div>
+      <div className="mb-14 flex items-center justify-between">
+        <h1 className="text-3xl font-medium text-richblack-5">My Courses</h1>
+        <IconBtn
+          text="Add Course"
+          onclick={() => navigate("/dashboard/add-course")}
+        >
+          <VscAdd />
+        </IconBtn>
+      </div>
+      {courses && <CoursesTable courses={courses} setCourses={setCourses} />}
+    </div>
+  )
+}
